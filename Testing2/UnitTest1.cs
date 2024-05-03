c4df6683f73d5220d49622ae777618602cd9aa36
Theo2717307 <P2717307@my365.dmu.ac.uk>
2024-05-03 11:27:35 +0100

    Added testing for order class

---
+++ Testing2/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create instance of a new class
+            clsOrder AnOrder = new clsOrder();
+            //test to see if it exists
+            Assert.IsNotNull( AnOrder );
+        }
+    }
+}
