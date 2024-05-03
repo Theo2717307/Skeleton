2a2624e7e6abd9eaa7e964d8eb7bacdfefdd97c1
p2717568 <p2717568@my365.dmu.ac.uk>
2024-05-03 11:51:14 +0100

    Added testing for staff class

---
+++ Testing3/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnAddress = new clsStaff();
+            //test to see thatit exists
+            Assert.IsNotNull(AnAddress);
+        }
+    }
+}
