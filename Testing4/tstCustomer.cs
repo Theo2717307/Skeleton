43e182217fada371c4dc42d8670678d6ae643d35
Sahil Suresh <P2716927@LEC-ADMIN.dmu.ac.uk>
2024-05-07 12:31:14 +0100

    Added property
    I have added some proeprty for each field on my table and my a test plan to check is property is okay

---
+++ Testing4/tstCustomer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstCustomer
+
+    {
+        [TestMethod]
+        public void instanceOK()
+        {
+            clsCustomer anCustomer = new clsCustomer();
+            Assert.IsNotNull(anCustomer);
+        }
+
+        [TestMethod]
+        public void CustomerIdPropertyOK() {
+            clsCustomer anCustomer = new clsCustomer();
+            Int32 TestData = 1;
+            anCustomer.CustomerId = TestData;
+            Assert.AreEqual(anCustomer.CustomerId, TestData);
+        }
+    }
+}
