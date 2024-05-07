5ade78fe2698346b8aa138ef1484c1d9ba071ae5
P2716769 <P2716769@my365.dmu.ac.uk>
2024-05-07 12:35:31 +0100

    Testing for clsSupply all table properties

---
+++ ClassLibrary/clsSupply.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassLibrary
+{
+    public class clsSupply
+    {
+        public clsSupply()
+        {
+        }
+
+        public string Supplier_Name { get; set; }
+        public string Supplier_Phone_Number { get; set; }
+        public string Supplier_Address { get; set; }
+        public string Supplier_Email { get; set; }
+        public string Supplier_Country { get; set; }
+        public bool Supplier_Export { get; set; }
+        public bool Supplier_Trade_Restrictions { get; set; }
+    }
+}
