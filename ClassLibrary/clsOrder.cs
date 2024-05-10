72add0656eff71301ae6e3a7010a82aa2acc609d
P2717307 <P2717307@my365.dmu.ac.uk>
2024-05-10 13:54:18 +0100

    Added the database integration for order class tests

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -55,7 +55,7 @@ namespace ClassLibrary
             }
         }
 
-        public bool Find(int testOrderID)
+        public bool Find_old(int testOrderID)
         {
             //set the private data members to the test data value
             mOrder_id = 15;
@@ -64,6 +64,32 @@ namespace ClassLibrary
             mOrder_processed = true;
             //always return true
             return true;
+        }
+        public bool Find(int orderID)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the address id to search for
+            DB.AddParameter("@orderID", orderID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblOrder_FilterByOrderId");
+            //if one record is found (there should be one or zero)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mOrder_id = Convert.ToInt32(DB.DataTable.Rows[0]["order_id"]);
+                mCustomer_id = Convert.ToInt32(DB.DataTable.Rows[0]["customer_id"]);
+                mOrder_timestamp = Convert.ToDateTime(DB.DataTable.Rows[0]["order_timestamp"]);
+                mOrder_processed = Convert.ToBoolean(DB.DataTable.Rows[0]["order_processed"]);
+                //return that everything is OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return a false indicating there is a 
+                return false;
+            }
         }
     }
 }
