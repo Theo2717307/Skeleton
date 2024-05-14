97998dc4263c0c47f7e3e487fa5cc278c0eb41f5
Sahil Suresh <P2716927@LEC-ADMIN.dmu.ac.uk>
2024-05-14 18:42:49 +0100

    Added validatoin to the form

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -34,7 +34,7 @@ public partial class _1_DataEntry : System.Web.UI.Page
         clsCustomer AnCustomer = new clsCustomer();
 
         //capture CustomerId
-        AnCustomer.Customer_Id = Convert.ToInt32(txtCustomer_Id.Text);
+        /*AnCustomer.Customer_Id = Convert.ToInt32(txtCustomer_Id.Text);
 
         //capture firstName 
         AnCustomer.Customer_FirstName = txtCustomer_FirstName.Text;
@@ -55,13 +55,55 @@ public partial class _1_DataEntry : System.Web.UI.Page
         AnCustomer.Customer_Password = txtCustomer_Password.Text;
 
         //capture active box
-        AnCustomer.Active = CheckBox1.Checked;
+        AnCustomer.Active = CheckBox1.Checked;*/
 
-        //store the address in the sessoin object
-        Session["AnCustomer"] = AnCustomer;
+        //Capturing user input
+        string Customer_FirstName = txtCustomer_FirstName.Text;
+        string Customer_LastName = txtCustomer_LastName.Text;
+        string Customer_PhoneNumber = txtCustomer_PhoneNumber.Text; 
+        string Customer_Email = txtCustomer_Email.Text;
+        string Customer_Password = txtCustomer_Password.Text; 
+        string Customer_Address = txtCustomer_Address.Text;
 
-        //navigate to the view page
-        Response.Redirect("CustomerViewer.aspx");
+        //variable to store any error messages
+        string Error = "";
+
+        //validation the data
+        Error = AnCustomer.Valid(Customer_FirstName, Customer_LastName, Customer_PhoneNumber, Customer_Email, Customer_Password, Customer_Address);
+        if(Error == "")
+        {
+            //capture the firstname
+            AnCustomer.Customer_FirstName = Customer_FirstName;
+
+            //Capture the lastname
+            AnCustomer.Customer_LastName = Customer_LastName;
+
+            //Capture the PhoneNumber
+            AnCustomer.Customer_PhoneNumber = Customer_PhoneNumber;
+
+            //Capture the Email
+            AnCustomer.Customer_Email= Customer_Email;
+
+            //Capture the Password
+            AnCustomer.Customer_Password= Customer_Password;   
+
+            //Capture the Address
+            AnCustomer.Customer_Address = Customer_Address;
+            //store the address in the sessoin object
+            Session["AnCustomer"] = AnCustomer;
+
+            //navigate to the view page
+            Response.Redirect("CustomerViewer.aspx");
+        }
+        else
+        {
+            //display the error message
+            lblErrorId.Text = Error;
+
+
+        }
+
+        
     }
 
     protected void btnFind_Click(object sender, EventArgs e)
