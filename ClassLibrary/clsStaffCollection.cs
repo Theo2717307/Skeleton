2dba82d81d7813f4345bd3413ce6186f3447d485
p2717568 <p2717568@my365.dmu.ac.uk>
2024-05-15 16:35:17 +0100

    created collection class

---
+++ ClassLibrary/clsStaffCollection.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System; // Import the System namespace
+
+
+namespace ClassLibrary
+{
+    public class clsStaffCollection
+    {
+        List<clsStaff> mStaffList = new List<clsStaff>();
+
+        //public property for the staff list
+
+        public List<clsStaff> StaffList
+        {
+            get
+            {
+                //set the private data
+                return mStaffList;
+
+            }
+            set
+            {
+                //set the private data
+                mStaffList = value;
+            }
+        }
+
+        public int Count
+        {
+            get 
+            { 
+                return mStaffList.Count; 
+            }
+
+            set
+            { ////later}
+
+            }
+        }
+        public clsStaff ThisStaff { get; set; }
+
+        public clsStaffCollection()
+        {
+            //constructor for the class
+            
+            //variable for the index
+            Int32 Index = 0;
+            //variable to store the record count
+            Int32 RecordCount = 0;
+            //object for the data connect
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount ) 
+            { 
+                //create a blank staffmember
+                clsStaff AnStaff = new clsStaff();
+                AnStaff.IsManager = Convert.ToBoolean(DB.DataTable.Rows[Index]["IsManager"]);
+                AnStaff.Staff_ID = Convert.ToInt32(DB.DataTable.Rows[Index]["Staff_ID"]);
+                AnStaff.StartDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["StartDate"]);
+                AnStaff.FirstName = Convert.ToString(DB.DataTable.Rows[Index]["FirstName"]);
+                AnStaff.LastName = Convert.ToString(DB.DataTable.Rows[Index]["LastName"]);
+                AnStaff.Position = Convert.ToString(DB.DataTable.Rows[Index]["Position"]);
+                AnStaff.Department = Convert.ToString(DB.DataTable.Rows[Index]["Department"]);
+                //add the record to the private data member
+                mStaffList.Add( AnStaff );
+                //point at the next record
+                Index++;
+            }
+ 
+        }
+       
+
+    }
+}
---
+++ Testing3/tstStaffCollection.cs
@@ -0,0 +1,103 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System; // Import the System namespace
+
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllStaff);
+
+        }
+
+        [TestMethod]
+        public void StaffListOK()
+        {
+            //create an instance of the class we want to creater
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some tst data to assign to the propery
+            //data needs to be a list of objects
+            List<clsStaff> TestList = new List<clsStaff>();
+            //add an item to the list
+            //create the item of test data
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.IsManager = true;
+            TestItem.Staff_ID = 1;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Wick";
+            TestItem.StartDate = DateTime.Now;
+            TestItem.Department = "Luxury";
+            TestItem.Position = "Assistant";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaff.StaffList = TestList;
+            //test to o se that the two values are the same
+            Assert.AreEqual(AllStaff.StaffList, TestList);
+
+
+
+        }
+
+     
+        [TestMethod]
+        public void ThisStaffPropertyOK() 
+        {
+            //create instance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //creat some test data to assing to the property
+            clsStaff TestStaff = new clsStaff();
+            //set the properties of the test objects
+            TestStaff.IsManager = true;
+            TestStaff.FirstName = "John";
+            TestStaff.LastName = "Wick";
+            TestStaff.Department = "Luxury";
+            TestStaff.Position = "Assistant";
+            TestStaff.StartDate = DateTime.Now;
+            TestStaff.Staff_ID = 1;
+            //assign the data to the property
+            AllStaff.ThisStaff = TestStaff;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.ThisStaff, TestStaff);
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an isntance of the class we want to create
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsStaff> TestList = new List<clsStaff>();
+            //add an item to the list
+            //create the item of test data
+            clsStaff TestItem = new clsStaff();
+            //set its properties
+            TestItem.IsManager = true;
+            TestItem.Staff_ID = 1;
+            TestItem.FirstName = "John";
+            TestItem.LastName = "Wick";
+            TestItem.StartDate = DateTime.Now;
+            TestItem.Department = "Luxury";
+            TestItem.Position = "Assistant";
+            //add the item to the test lists
+            TestList.Add(TestItem);
+            //assign the data to the propert
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.Count, TestList.Count);
+
+        }
+    
+    }
+}
