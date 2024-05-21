98c20df8109f2bd788c433da5fd2c1fc6aa185a7
P2716945 <P2716945@my365.dmu.ac.uk>
2024-05-21 12:13:56 +0100

    fixed all errors

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,9 +1,19 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
         public clsStock()
         {
         }
+
+        public int quantity { get; set; }
+        public int unit_price { get; set; }
+        public int product_id { get; set; }
+        public string stockName { get; set; }
+        public string stockDetails { get; set; }
+        public DateTime last_restock_date { get; set; }
+        public bool discontinued { get; set; }
     }
 }
