a7dcfb165009c0d2e7315b9d8c79a116871b2016
SahilSuresh <getshahilsuresh@gmail.com>
2024-05-23 20:42:47 +0100

    Create simple login page

---
+++ AdminSystem/CustomerLogin.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CustomerLogin.aspx.cs" Inherits="CustomerLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="lblUserName" runat="server" style="z-index: 1; left: 119px; top: 158px; position: absolute; width: 70px" Text="UserName:"></asp:Label>
+        <asp:TextBox ID="txtPassword" runat="server" style="z-index: 1; left: 203px; top: 204px; position: absolute" TextMode="Password"></asp:TextBox>
+        <asp:Label ID="lblUserName0" runat="server" style="z-index: 1; left: 119px; top: 158px; position: absolute; width: 70px" Text="UserName:"></asp:Label>
+        <asp:TextBox ID="txtUserName" runat="server" style="z-index: 1; left: 204px; top: 158px; position: absolute"></asp:TextBox>
+        <asp:Label ID="lblPassword" runat="server" style="z-index: 1; left: 120px; top: 203px; position: absolute" Text="Password:"></asp:Label>
+        <p>
+            <asp:Button ID="btnLogin" runat="server" OnClick="btnLogin_Click" style="z-index: 1; left: 143px; top: 246px; position: absolute" Text="Login" />
+        </p>
+        <asp:Button ID="btnCancel" runat="server" style="z-index: 1; left: 210px; top: 245px; position: absolute" Text="Cancel" />
+        <p>
+            <asp:Label ID="lblError" runat="server" ForeColor="Red" style="z-index: 1; left: 415px; top: 195px; position: absolute"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Testing4/tstCustomerUser.cs
@@ -0,0 +1,114 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstCustomerUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class 
+            clsCustomerUser anUser = new clsCustomerUser();
+            //test to see that it exist
+            Assert.IsNotNull(anUser);
+        }
+
+        [TestMethod]
+        public void UserIdPropertyOK()
+        {
+            //create an instance of the class 
+            clsCustomerUser anUser = new clsCustomerUser();
+            //create some test data
+            Int32 TestData = 1;
+            //assign the data to the property
+            anUser.UserId = TestData;
+            //test to see that it exist
+            Assert.AreEqual(anUser.UserId, TestData);
+        }
+
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            string TestData = "Dawn";
+            //assign the data to the property
+            AnUser.UserName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.UserName, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            string TestData = "password123";
+            //assign the data to the property
+            AnUser.Password = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Password, TestData);
+        }
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            string TestData = "Address Book";
+            //assign the data to the property
+            AnUser.Department = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+
+        [TestMethod]
+        public void FindUserMethodOK()
+        {
+            //create an instance of class
+            clsCustomerUser anUser = new clsCustomerUser();
+            //create a boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the methods
+            string UserName = "Dawn";
+            string Password = "pass124";
+            //invoke the method
+            Found = anUser.FindUser(UserName, Password);
+            Assert.IsTrue(Found);  
+
+        }
+
+        [TestMethod]
+
+        public void TestUserNamePWFound()
+        {
+            //create an instance of the class 
+            clsCustomerUser anUser = new clsCustomerUser();
+            //Create vriable to store the result of the search
+            Boolean Found = false;
+            //create a Boolean variable to record if the data is Ok
+            Boolean OK = true;
+            //create some test data to use with the methods
+            string UserName = "Dawn";
+            string Password = "pass124";
+            //invoke the methods
+            Found = anUser.FindUser(UserName, Password);
+            //check the user Id property
+            if (anUser.UserName != UserName && anUser.Password != Password) 
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+
+        }
+
+    }
+
+}
