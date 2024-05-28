c0147156d51aa065cfabe5cc278bc33af6be4d01
SahilSuresh <160167504+SahilSuresh@users.noreply.github.com>
2024-05-28 18:50:04 +0100

    Update CustomerLogin.aspx.cs
    added the return to the main menu on the cancel button on customer login page

--- AdminSystem/CustomerLogin.aspx.cs
+++ AdminSystem/CustomerLogin.aspx.cs
@@ -50,4 +50,9 @@ public partial class CustomerLogin : System.Web.UI.Page
             lblError.Text = "Login details are incorrect! Please try again ";
         }
     }
+     protected void btnCancel_Click(object sender, EventArgs e)
+ {
+     Response.Redirect("TeamMainMenu.aspx");
+
+ }
 }
